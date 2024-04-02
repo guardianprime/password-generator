8aeb98f3fb020ef03a2e040af2ceb6c9f14dc8d6
Guardianprime <guardianacer@outlook.com>
2024-04-02 16:16:15 +0100

    optimezed the code again cause of some slow behaviours on the app

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-const btnEl = document.querySelector(".btn")
-const inputEl = document.querySelector(".input")
-const copyIconEl = document.querySelector(".fa-copy")
-const alertContainerEl = document.querySelector(".alert-container")
+document.addEventListener("DOMContentLoaded", () => {
+    const char = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(){}[]?|/";
+    const passwordLength = 16;
 
-btnEl.addEventListener("click", () => {
-    generatePassword()
-})
+    const generatePassword = () => {
+        let password = "";
+        for (let i = 0; i < passwordLength; i++) {
+            const randomNumber = Math.floor(Math.random() * char.length);
+            password += char[randomNumber];
+        }
+        inputEl.value = password;
+        alertContainerEl.innerText = `${password} Copied!`;
+    };
 
-copyIconEl.addEventListener("click", () => {
-    copyPassword()
-    if (inputEl.value) {
-        alertContainerEl.classList.remove("active")
-        setTimeout(() => {
-            alertContainerEl.classList.add("active")
-        }, 2000);
-    }
-})
-function generatePassword() {
-    const char = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(){}[]?|/"
-    let password = ""
-    const passwordLength = 16
+    const copyPassword = () => {
+        inputEl.select();
+        inputEl.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(inputEl.value);
+    };
 
-    for (let i = 0; i < passwordLength; i++) {
-        const randomNumber = Math.floor(Math.random() * char.length)
-        password += char.substring(randomNumber, randomNumber + 1)
-        console.log(randomNumber, password)
-    }
-    inputEl.value = password
-    alertContainerEl.innerText = `${password} Copied!`
-}
+    const btnEl = document.querySelector(".btn");
+    const inputEl = document.querySelector(".input");
+    const copyIconEl = document.querySelector(".fa-copy");
+    const alertContainerEl = document.querySelector(".alert-container");
 
-function copyPassword() {
-    inputEl.select()
-    inputEl.setSelectionRange(0, 9999)
-    navigator.clipboard.writeText(inputEl.value)
-}
+    btnEl.addEventListener("click", generatePassword);
+    copyIconEl.addEventListener("click", () => {
+        copyPassword();
+        if (inputEl.value) {
+            alertContainerEl.classList.remove("active");
+            setTimeout(() => {
+                alertContainerEl.classList.add("active");
+            }, 3000);
+        }
+    });
+});
